fa9150448a82dd2886f2444659c9c1e171d9fa43
jadfeitrouni <jadfeitrouni@gmail.com>
2015-09-24 19:34:36 +0300

    Initial Commit

---
+++ JFMoviePlayerTests/JFMoviePlayerTests.m
@@ -0,0 +1,40 @@
+//
+//  JFMoviePlayerTests.m
+//  JFMoviePlayerTests
+//
+//  Created by Phoenikia on 9/24/15.
+//  Copyright (c) 2015 jad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface JFMoviePlayerTests : XCTestCase
+
+@end
+
+@implementation JFMoviePlayerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
